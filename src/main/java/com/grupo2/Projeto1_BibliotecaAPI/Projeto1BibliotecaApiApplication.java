593f5187eba55e955f180f7e2559cc080c31bc12
rafaelspa <rsdepaula67@gmail.com>
2023-05-05 13:17:08 -0300

    Start

---
+++ src/main/java/com/grupo2/Projeto1_BibliotecaAPI/Projeto1BibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.Projeto1_BibliotecaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto1BibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1BibliotecaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo2/Projeto1_BibliotecaAPI/Projeto1BibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.Projeto1_BibliotecaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1BibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
